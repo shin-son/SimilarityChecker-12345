402ba6cb07e3805f28b2584bccc37234099ecae5
shin.son <thstls110534@naver.com>
2025-07-02 15:25:15 +0900

    [feature] 02

--- day08-Similarty-checker/main.cpp
+++ day08-Similarty-checker/main.cpp
@@ -6,6 +6,11 @@ TEST(SIMILARITYCHECK, compareEqualString) {
 	EXPECT_EQ(100, stringComparator.compareString("ABCDE", "ABCDE"));
 }
 
+TEST(SIMILARITYCHECK, compareLength) {
+	StringComparator stringComparator;
+	EXPECT_EQ(60, stringComparator.compareString("FGHIJ", "ABCDE"));
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
--- day08-Similarty-checker/similarity_checker.h
+++ day08-Similarty-checker/similarity_checker.h
@@ -7,5 +7,20 @@ class StringComparator {
 public:
 	int compareString(const string& string1, const string& string2) {
 		if (string1 == string2) return 100;
+		int gap;
+		double percentage, partialPoint;
+
+		if (string1.size() > string2.size()) {
+			gap = string1.size() - string2.size();
+			percentage = gap / string2.size();
+			partialPoint = (1 - percentage) * 60;
+		}
+		else {
+			gap = string2.size() - string1.size();
+			percentage = gap / string1.size();
+			partialPoint = (1 - percentage) * 60;
+		}
+
+		return static_cast<int>(partialPoint);
 	}
 };
