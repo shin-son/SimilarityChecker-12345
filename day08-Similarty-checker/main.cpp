6f30c1e653a0bffbd47610afc1b335f2a29f9fe8
shin.son <thstls110534@naver.com>
2025-07-02 15:48:36 +0900

    [refactoring] 01

--- day08-Similarty-checker/main.cpp
+++ day08-Similarty-checker/main.cpp
@@ -1,28 +1,30 @@
 #include "gmock/gmock.h"
 #include "similarity_checker.h"
 
-TEST(SIMILARITYCHECK, compareEqualString) {
+using namespace testing;
+
+class stringComparatorFixture : public Test {
+public:
 	StringComparator stringComparator;
+};
+
+TEST_F(stringComparatorFixture, compareEqualString) {
 	EXPECT_EQ(100, stringComparator.compareString("ABCDE", "ABCDE"));
 }
 
-TEST(SIMILARITYCHECK, compareLength) {
-	StringComparator stringComparator;
+TEST_F(stringComparatorFixture, compareLength) {
 	EXPECT_EQ(60, stringComparator.compareString("FGHIJ", "ABCDE"));
 }
 
-TEST(SIMILARITYCHECK, compareLengthMoreThanSixStrings) {
-	StringComparator stringComparator;
+TEST_F(stringComparatorFixture, compareLengthMoreThanSixStrings) {
 	EXPECT_EQ(48, stringComparator.compareString("FGHIJK", "ABCDE"));
 }
 
-TEST(SIMILARITYCHECK, compareLengthLessThanFourStrings) {
-	StringComparator stringComparator;
+TEST_F(stringComparatorFixture, compareLengthLessThanFourStrings) {
 	EXPECT_EQ(45, stringComparator.compareString("FGHI", "ABCDE"));
 }
 
-TEST(SIMILARITYCHECK, compareLengthLessThanOnetring) {
-	StringComparator stringComparator;
+TEST_F(stringComparatorFixture, compareLengthLessThanOnetring) {
 	EXPECT_EQ(0, stringComparator.compareString("F", "ABCDE"));
 }
 
